08e988878f9da2cf342649cf7c98593788c6756b
Kristina Markina <kr.markina@gmail.com>
2021-05-12 14:38:56 -0400

    overwrite the migration with polymorphic

--- db/migrate/20210512172127_add_production_to_actor.rb
+++
@@ -1,6 +0,0 @@
-class AddProductionToActor < ActiveRecord::Migration[5.0]
-  def change
-    add_column :actor, :production_id, :integer
-    add_column :actor, :production_type, :string
-  end
-end
